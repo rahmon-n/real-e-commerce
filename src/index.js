288b19bf9e636ea27a84e674fddb3bb3e4561dc1
Rahmon <rahmon.dev@gmail.comgit config --global user.email rahmon.dev@gmail.comgit config --global user.email rahmon.dev@gmail.com>
2022-05-10 16:15:25 +0500

    broken our homepage into smaller components, finished styling our menu-item component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
