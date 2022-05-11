022e9e2d7d8aeadab944346a55e0a3f5da03ea6f
Rahmon <rahmon.dev@gmail.comgit config --global user.email rahmon.dev@gmail.comgit config --global user.email rahmon.dev@gmail.com>
2022-05-11 23:46:34 +0500

    верстка главной страницы и начало осуществления ровтинга

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { AboutPage } from './pages/about.component';
 import { HomePage } from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Directory from '../../components/directory-menu/directory.component';
 import './homepage.styles.scss';
 
@@ -6,6 +7,7 @@ export const HomePage = () => {
   return (
     <div className="homepage">
       <Directory />
+      <Link to={'/about'}>About</Link>
     </div>
   );
 };
